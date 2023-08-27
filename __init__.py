78252e5a27c04131f8f32de04e42fa297f537989
Sungjae Jang <30320274+eaj3@users.noreply.github.com>
2023-08-27 17:09:50 +1200

    Update __init__.py
    Add docstring.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,20 @@
+"""
+AdversarialInsightML
 
+This is a Python package that provides evaluation of ML models based on adversarial attacks.
+
+Usage:
+    import AdversarialInsightML
+
+    # Now you can use the functions and classes from the package.
+    # Example: 
+
+Modules:
+- attacks: Tools for generating adversarial attacks and evaluating their effectiveness.
+
+For detailed documentation, usage examples, and guidelines, please refer to the official
+documentation at [LINK].
+
+Author: Team7
+Version: 
+"""
