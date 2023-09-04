dc6321186dd243535e040fa57b59886f5d047d2c
Sungjae Jang <jwj981417@gmail.com>
2023-09-05 01:57:00 +1200

    Relocate user_functions.py

--- src/aiml/user_functions.py
+++ src/aiml/user_functions.py
@@ -1,26 +1,71 @@
-""" THIS DOCSTRING IS A PLACEHOLDER AND NEEDS UPDATE
+import torch
 
-User Functions Module
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from art.estimators.classification import PyTorchClassifier
 
-This module provides a collection of user-friendly functions to simplify the usage of AIML. 
-It serves as an entry point for users looking to perform common tasks in a streamlined way.
+def user_called_function(input_model,input_train_data=None,input_test_data=None,input_shape=None,
+                         clip_values=None,nb_classes=None,batch_size_attack = 64,num_threads_attack= 8,batch_size_train = 64,batch_size_test = 64):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = load_model(input_model)            
+    if input_train_data != None:
+      dataset_train = load_set(input_train_data)
+      dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=batch_size_train, shuffle=False)
+    if input_test_data == None:
+      print("please input test_data")
+    
+    dataset_test = load_set(input_test_data)
+    dataloader_test = torch.utils.data.DataLoader(dataset_test,batch_size=batch_size_test, shuffle=False)
+    
+    if input_shape == None:
+        (x, y) = next(iter(dataset_test))
+        input_shape = np.array(x.size())
+        print(f'input_shape: {input_shape}')
 
-Functions:  
-    - `load_data(file_path)`: Load data from a file and return it in a structured format.
-    - `preprocess_data(data)`: Perform data preprocessing, including cleaning and feature engineering.
-    - `train_model(data, target)`: Train a machine learning model on the provided data.
-    - `evaluate_model(model, test_data, test_target)`: Evaluate the model's performance on a test dataset.
-    - `generate_predictions(model, new_data)`: Generate predictions using a trained model.
+    if clip_values == None:
+        global_min = 9999.
+        global_max = 0.
+        s = 0
+        n = 0
+        b = True
+        for batch in dataloader_train:
+            x, _ = batch
+            if b == True:
+                print(batch)
+                b = False
+            global_min = min(torch.min(x).item(), global_min)
+            global_max = max(torch.max(x).item(), global_max)
+        clip_values = (global_min,global_max)
 
-Usage Example:
-    >>> import your_package.user_functions as uf
-    >>> data = uf.load_data('data.csv')
-    >>> data = uf.preprocess_data(data)
-    >>> model = uf.train_model(data, target='label')
-    >>> test_data = uf.load_data('test_data.csv')
-    >>> predictions = uf.generate_predictions(model, test_data)
+        print(f'Min: {global_min}, Max: {global_max}')
+    if nb_classes == None:
+        list1 = []
+        for i in range(len(dataset_test)):
+            x,y = dataset_test[i]
+            if y not in list1:
+                list1+=[y]
+        nb_classes=len(dataset_train)
+    if input_train_data!=None:
+      acc_train = test_accuracy(model, dataloader_train, device)
+      print(f'Train accuracy: {acc_train * 100:.2f}')
+    acc_test = test_accuracy(model, dataloader_test, device)
+    print(f'Test accuracy:  {acc_test * 100:.2f}')
+    classifier = PyTorchClassifier(
+        model=model,
+        clip_values=clip_values, 
+        loss=None,
+        optimizer=None,
+        input_shape=input_shape,
+        nb_classes=nb_classes,
+    )
+    result_list = test_all_attack(classifier,dataloader_test,batch_size_attack,num_threads_attack,device)
+    evaluate(result_list)
 
-For more detailed information on each function and its parameters, please refer to the documentation for individual functions within this module.
 
-"""
+
+
+
+
 
