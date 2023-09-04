b987e029574f0a13cf29e9fac46c6026a05795bb
Sungjae Jang <30320274+eaj3@users.noreply.github.com>
2023-09-04 17:36:02 +1200

    Update and relocate user_functions.py to ./src/aiml

---
+++ src/aiml/user_functions.py
@@ -0,0 +1,26 @@
+""" THIS DOCSTRING IS A PLACEHOLDER AND NEEDS UPDATE
+
+User Functions Module
+
+This module provides a collection of user-friendly functions to simplify the usage of AIML. 
+It serves as an entry point for users looking to perform common tasks in a streamlined way.
+
+Functions:  
+    - `load_data(file_path)`: Load data from a file and return it in a structured format.
+    - `preprocess_data(data)`: Perform data preprocessing, including cleaning and feature engineering.
+    - `train_model(data, target)`: Train a machine learning model on the provided data.
+    - `evaluate_model(model, test_data, test_target)`: Evaluate the model's performance on a test dataset.
+    - `generate_predictions(model, new_data)`: Generate predictions using a trained model.
+
+Usage Example:
+    >>> import your_package.user_functions as uf
+    >>> data = uf.load_data('data.csv')
+    >>> data = uf.preprocess_data(data)
+    >>> model = uf.train_model(data, target='label')
+    >>> test_data = uf.load_data('test_data.csv')
+    >>> predictions = uf.generate_predictions(model, test_data)
+
+For more detailed information on each function and its parameters, please refer to the documentation for individual functions within this module.
+
+"""
+
