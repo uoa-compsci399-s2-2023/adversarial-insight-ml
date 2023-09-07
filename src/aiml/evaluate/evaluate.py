3fdb98a1d64e74b85ad7bd390de7fc3284c9554a
Sungjae Jang <30320274+eaj3@users.noreply.github.com>
2023-09-07 16:21:42 +1200

    Update evaluate.py

--- src/aiml/evaluate/evaluate.py
+++ src/aiml/evaluate/evaluate.py
@@ -5,11 +5,13 @@ This module provides the evaluate function which will call functions to apply al
 and determine a risk evaluation based on them (low, medium, high.)
 """
 
-def evaluate(result_list):
+def evaluate(input_model, input_train_data=None, input_test_data=None, input_shape=None, clip_values=None, nb_classes=None, 
+             batch_size_attack=64, num_threads_attack=8, batch_size_train=64, batch_size_test=64):
     # Call other modules to perform attacks and receive accuracy
+    risk_eval = "HIGH"
+    result_list = get_accuracy_results(input_model, input_train_data=None, input_test_data=None, input_shape=None, clip_values=None, nb_classes=None, 
+             batch_size_attack=64, num_threads_attack=8, batch_size_train=64, batch_size_test=64)
     
     # Algorithm to weigh and calculate differnt accuracy scores, to give a final risk evaluation 
-    print(result_list)
-    risk_eval = "LOW"
     
     return risk_eval
