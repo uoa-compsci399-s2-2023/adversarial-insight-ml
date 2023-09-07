7ca883de10d6c1f49197d54143370b2ab2201d9e
Sungjae Jang <30320274+eaj3@users.noreply.github.com>
2023-09-07 14:57:43 +1200

    Update and rename test_all_white_box_attack.py to test_white_box.py

--- src/aiml/test_all_white_box_attack/test_white_box.py
+++ src/aiml/test_all_white_box_attack/test_white_box.py
@@ -1,3 +1,7 @@
+"""
+test_white_box.py
+"""
+
 def test_white_box_attack(attack_method,model,PyTorchClassifier,dataloader_test,batch_size_attack,num_threads_attack,device):
     attack=attack_method(estimator=PyTorchClassifier)
     batch = next(iter(dataloader_test))
@@ -7,8 +11,9 @@ def test_white_box_attack(attack_method,model,PyTorchClassifier,dataloader_test,
     dataloader_advx = DataLoader(dataset_advx, batch_size=batch_size_attack, shuffle=False, num_workers=num_threads_attack)
     acc_advx = evaluation(model, dataloader_advx, device)
     return (acc_advx * 100)
+    
 def test_all_white_box_attack(model,PyTorchClassifier,dataloader_test,batch_size_attack,num_threads_attack,device):
-    attack_method_list=[adversarial_patch,carlini_l0_method,deep_fool]
+    attack_method_list = [adversarial_patch,carlini_l0_method,deep_fool]
     accuracy_list=[]
     for attack_method in attack_method_list:
         accuracy_list+=[test_white_box_attack(adversarial_patch,model,PyTorchClassifier,dataloader_test,batch_size_attack,num_threads_attack,device)]
