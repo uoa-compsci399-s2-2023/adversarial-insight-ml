de1d15a1a1a43d7058337da0936fb4c0ed6ae454
zhou920ming <83534031+zhou920ming@users.noreply.github.com>
2023-09-07 09:17:32 +0800

    Update test_all_white_box_attack.py

--- src/aiml/test_all_white_box_attack/test_all_white_box_attack.py
+++ src/aiml/test_all_white_box_attack/test_all_white_box_attack.py
@@ -8,8 +8,10 @@ def test_white_box_attack(attack_method,model,PyTorchClassifier,dataloader_test,
     acc_advx = evaluation(model, dataloader_advx, device)
     return (acc_advx * 100)
 def test_all_white_box_attack(model,PyTorchClassifier,dataloader_test,batch_size_attack,num_threads_attack,device):
-    attack_method_list=[adversarial_patch
-    test_white_box_attack(adversarial_patch,model,PyTorchClassifier,dataloader_test,batch_size_attack,num_threads_attack,device)
+    attack_method_list=[adversarial_patch,carlini_l0_method,deep_fool]
+    accuracy_list=[]
+    for attack_method in attack_method_list:
+        accuracy_list+=[test_white_box_attack(adversarial_patch,model,PyTorchClassifier,dataloader_test,batch_size_attack,num_threads_attack,device)]
 
 
     
