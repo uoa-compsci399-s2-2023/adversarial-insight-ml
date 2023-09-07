64bf3c1cf761a0aaa0a2dc60135f7a6e138bf9f6
zhou920ming <83534031+zhou920ming@users.noreply.github.com>
2023-09-07 12:33:56 +0800

    Update evaluate.py

--- src/aiml/evaluate/evaluate.py
+++ src/aiml/evaluate/evaluate.py
@@ -4,7 +4,7 @@ evaluate.py
 This module provides the evaluate function which will call functions to apply all appropriate attacks, 
 and determine a risk evaluation based on them (low, medium, high.)
 """
-
+from get_accuracy_results import*
 def evaluate(input_model, input_train_data=None, input_test_data=None, input_shape=None, clip_values=None, nb_classes=None, 
              batch_size_attack=64, num_threads_attack=8, batch_size_train=64, batch_size_test=64):
     # Call other modules to perform attacks and receive accuracy
