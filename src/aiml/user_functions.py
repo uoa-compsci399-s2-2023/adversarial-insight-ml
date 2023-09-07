3dc9c3085a2221d3b1e92b4fd6b4aadaf582400e
zhou920ming <83534031+zhou920ming@users.noreply.github.com>
2023-09-07 09:28:26 +0800

    Update user_functions.py

--- src/aiml/user_functions.py
+++ src/aiml/user_functions.py
@@ -17,36 +17,8 @@ def user_called_function(input_model,input_train_data=None,input_test_data=None,
     
     dataset_test = load_set(input_test_data)
     dataloader_test = torch.utils.data.DataLoader(dataset_test,batch_size=batch_size_test, shuffle=False)
+    input_shape,clip_values,nb_classes=generate_parameter(input_shape,clip_values,nb_classes)
     
-    if input_shape == None:
-        (x, y) = next(iter(dataset_test))
-        input_shape = np.array(x.size())
-        print(f'input_shape: {input_shape}')
-
-    if clip_values == None:
-        global_min = 9999.
-        global_max = 0.
-        s = 0
-        n = 0
-        b = True
-        for batch in dataloader_train:
-            x, _ = batch
-            if b == True:
-                print(batch)
-                b = False
-            global_min = min(torch.min(x).item(), global_min)
-            global_max = max(torch.max(x).item(), global_max)
-        clip_values = (global_min,global_max)
-
-        print(f'Min: {global_min}, Max: {global_max}')
-
-    if nb_classes == None:
-        list1 = []
-        for i in range(len(dataset_test)):
-            x,y = dataset_test[i]
-            if y not in list1:
-                list1+=[y]
-        nb_classes=len(dataset_train)
         
     if input_train_data!=None:
         acc_train = test_accuracy(model, dataloader_train, device)
