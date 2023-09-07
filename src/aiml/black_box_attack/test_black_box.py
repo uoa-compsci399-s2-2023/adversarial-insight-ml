904ae81913480b983e5fa9a6c36f4a4bc7273b30
zhou920ming <83534031+zhou920ming@users.noreply.github.com>
2023-09-07 12:25:47 +0800

    Update test_black_box.py

--- src/aiml/black_box_attack/test_black_box.py
+++ src/aiml/black_box_attack/test_black_box.py
@@ -1,7 +1,8 @@
 """
 test_black_box.py
 """
-
+from surrogate_model.create_surrogate_model import *
+from
 def black_box_test(black_box_model):
     surrogate_model=create_surrogate_model(black_box_model)
     result=test_all_white_box_attack(surrogate_model)
