bf8afbebcf438b654aa26f760e53a00487992d99
Sungjae Jang <jwj981417@gmail.com>
2023-09-15 06:32:40 +1200

    Update test_black_box.py

--- src/aiml/attack/test_black_box.py
+++ src/aiml/attack/test_black_box.py
@@ -7,5 +7,6 @@ from surrogate_model.create_surrogate_model import *
 from attack.test_white_box import *
 
 def black_box_test(black_box_model):
+    #TODO: Implement black-box attack testing
     surrogate_model = create_surrogate_model(black_box_model)
     result = test_all_white_box_attack(surrogate_model)
