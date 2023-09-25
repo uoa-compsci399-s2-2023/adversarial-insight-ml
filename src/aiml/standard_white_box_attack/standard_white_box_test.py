9c1f77d531ae42b21577bbd1f57468baf71a183e
Sungjae Jang <jwj981417@gmail.com>
2023-09-26 04:30:02 +1300

    Change attack methods used

--- src/aiml/standard_white_box_attack/standard_white_box_test.py
+++ src/aiml/standard_white_box_attack/standard_white_box_test.py
@@ -6,19 +6,13 @@ This module provides functions to create various adversarial attacks using the A
 
 
 from art.attacks.evasion import (
-    AutoProjectedGradientDescent,
-    CarliniL0Method,
-    CarliniL2Method,
-    CarliniLInfMethod,
-    DeepFool,
-    ElasticNet,
-    HopSkipJump,
-    NewtonFool,
     PixelAttack,
-    SaliencyMapMethod,
-    SquareAttack,
-    UniversalPerturbation,
+    AutoProjectedGradientDescent,
     ZooAttack,
+    CarliniL0Method,
+    CarliniLInfMethod,
+    CarliniL2Method,
+    SquareAttack
 )
 import numpy as np
 
@@ -48,15 +42,6 @@ def auto_projected_difference_logits_ratio(estimator, norm = np.inf, eps = 0.3, 
                     loss_type="cross_entropy",
                 )
 
-def deepfool_auto(estimator, batch_size = 32):
-    return DeepFool(
-                        classifier=estimator,  # type: ignore
-                        max_iter=100,
-                        epsilon=1e-3,
-                        nb_grads=10,
-                        batch_size=batch_size,
-                    )
-
 def square_attack_auto(estimator, norm = np.inf, eps = 0.3):
     return SquareAttack(estimator=estimator, norm=norm, max_iter=5000, eps=eps, p_init=0.8, nb_restarts=5)
 
@@ -78,48 +63,11 @@ def carlini_Linf_attack(classifier, confidence=0.0, targeted=False, learning_rat
     return CarliniLInfMethod(classifier, confidence, targeted, learning_rate, max_iter, decrease_factor, initial_const,
                              largest_const, const_factor, batch_size, verbose)
 
-def deep_fool_attack(classifier, max_iter=100, epsilon=1e-06, nb_grads=10, batch_size=1, verbose=True):
-    """
-    Deep_Fool takes in a classifier and returns an ART DeepFool instance.
-
-    Inputs: 
-    classifier: A trained classifier of type CLASSIFIER_CLASS_LOSS_GRADIENTS_TYPE.
-    max_iter (int): The maximum number of iterations.
-    epsilon (float): Overshoot parameter.
-    nb_grads (int): The number of class gradients (top nb_grads w.r.t. prediction) to compute. This way only the most likely classes are considered, speeding up the computation.
-    batch_size (int): Batch size
-    verbose (bool): Show progress bars.
-
-    Output:
-    DeepFool: A DeepFool instance from art.attacks.evasion
-    """
-    return DeepFool(classifier, max_iter, epsilon, nb_grads, batch_size, verbose)
-
-def elastic_net_attack(classifier, confidence=0.0, targeted=False, learning_rate=0.01, binary_search_steps=9,
-                      max_iter=100, beta=0.001, initial_const=0.001, batch_size=1, decision_rule='EN',
-                      verbose=True):
-    return ElasticNet(classifier, confidence, targeted, learning_rate, binary_search_steps, max_iter, beta,
-                      initial_const, batch_size, decision_rule, verbose)
-
-def hopskipjump_attack(classifier, batch_size=64, targeted=False, norm=2, max_iter=50, max_eval=10000,
-                      init_eval=100, init_size=100, verbose=True):
-    return HopSkipJump(classifier, batch_size, targeted, norm, max_iter, max_eval, init_eval, init_size, verbose)
-
-def newton_fool_attack(classifier, max_iter=100, eta=0.01, batch_size=1, verbose=True):
-    return NewtonFool(classifier, max_iter, eta, batch_size, verbose)
-
 def pixel_attack(classifier, th = None, es = 1, max_iter = 100, targeted = False, verbose = True):
     return PixelAttack(classifier, th, es, max_iter, targeted, verbose)
 
-def saliency_map_attack(classifier, theta=0.1, gamma=1.0, batch_size=1, verbose=True):
-    return SaliencyMapMethod(classifier, theta, gamma, batch_size, verbose)
-
 def square_attack(estimator, norm = np.inf, adv_criterion = None, loss = None, max_iter = 100, eps = 0.3, p_init = 0.8, nb_restarts = 1, batch_size = 128, verbose = True):
     return SquareAttack(estimator, norm, adv_criterion, loss, max_iter, eps, p_init, nb_restarts, batch_size, verbose)
-
-def universal_perturbation_attack(classifier, attacker='deepfool', attacker_params=None, delta=0.2, max_iter=20,
-                                  eps=10.0, norm=np.inf, batch_size=32, verbose=True):
-    return UniversalPerturbation(classifier, attacker, attacker_params, delta, max_iter, eps, norm, batch_size, verbose)
 
 def zoo_attack(classifier, confidence=0.0, targeted=False, learning_rate=0.01, max_iter=10, binary_search_steps=1,
                initial_const=0.001, abort_early=True, use_resize=True, use_importance=True, nb_parallel=128,
