c5fd6c56be4fc8188c4de44db59f5da8d63d7032
Sungjae Jang <jwj981417@gmail.com>
2023-09-26 23:26:10 +1300

    Format code

--- src/aiml/evaluate/evaluate.py
+++ src/aiml/evaluate/evaluate.py
@@ -1,29 +1,51 @@
 """
 evaluate.py
 
-This module provides the evaluate function which will call functions to apply all appropriate attacks, 
-and determine a risk evaluation based on them (low, medium, high.)
+This module provides the evaluate function which will call functions to 
+apply all appropriate attacks, and determine a risk evaluation based on 
+them (low, medium, high.)
 """
 
 
 from evaluate.get_accuracy_results import get_accuracy_results
 
+
 # Returns average in a percentage, of given float list
 def calculate_average(result_list):
     return (sum(result_list) / len(result_list)) / 100
 
+
 # Main function for evaluating a model
-def evaluate(input_model, input_train_data=None, input_test_data=None, input_shape=None, clip_values=None, 
-             nb_classes=None, batch_size_attack=64, num_threads_attack=8, batch_size_train=64, batch_size_test=64):
+def evaluate(
+    input_model,
+    input_train_data=None,
+    input_test_data=None,
+    input_shape=None,
+    clip_values=None,
+    nb_classes=None,
+    batch_size_attack=64,
+    num_threads_attack=8,
+    batch_size_train=64,
+    batch_size_test=64,
+):
     # Call other modules to perform attacks and receive accuracy
-    risk_levels = ['LOW', 'MEDIUM', 'HIGH']  # Risk levels to summarise our evaluation
+    risk_levels = ["LOW", "MEDIUM", "HIGH"]
     risk_eval = risk_levels[2]  # Default risk evaluation is high
 
-    result_list = get_accuracy_results(input_model, input_train_data, input_test_data, input_shape, clip_values, 
-                                       nb_classes, batch_size_attack, num_threads_attack, batch_size_train, 
-                                       batch_size_test)
-    
-    # Algorithm to weigh and calculate differnt accuracy scores, to give a final risk evaluation
+    result_list = get_accuracy_results(
+        input_model,
+        input_train_data,
+        input_test_data,
+        input_shape,
+        clip_values,
+        nb_classes,
+        batch_size_attack,
+        num_threads_attack,
+        batch_size_train,
+        batch_size_test,
+    )
+
+    # Algorithm to determine risk evaluation
     white_box_average = calculate_average(result_list)
     if white_box_average >= 90:
         risk_eval = risk_levels[0]
@@ -39,7 +61,7 @@ def evaluate(input_model, input_train_data=None, input_test_data=None, input_sha
         "Average accuracy for white box attacks: {:.1%}\n"
         "Risk level of the model is {}.\n"
     ).format(white_box_average, risk_eval)
-    
+
     print(evaluation_summary)
 
     return None
