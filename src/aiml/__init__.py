9477eb315c546f0ce52242c8e8eb618680960beb
Sungjae Jang <jwj981417@gmail.com>
2023-09-26 18:40:58 +1300

    Update __init__.py
    Remove standard_white_box_attack

--- src/aiml/__init__.py
+++ src/aiml/__init__.py
@@ -1,12 +1,9 @@
 """
+__init__.py
+
 Adversarial Insight ML
-
-This is a Python package that provides evaluation of ML models based on adversarial attacks.
-
-This module is the entry point for the package.
-
-Author: Team 7
-
+This is a Python package that provides evaluation of ML models based on 
+adversarial attacks. This module is the entry point for the package.
 """
 
 
@@ -14,7 +11,6 @@ Author: Team 7
 from .attack import *
 from .evaluate import *
 from .load_data import *
-from .standard_white_box_attack import *
 from .surrogate_model import *
 from .test_accuracy import *
 
@@ -23,7 +19,14 @@ __version__ = "0.0.3"
 __doc__ = "Adversarial Insight ML is a package that provides evaluation of ML models based on adversarial attacks."
 
 # List of symbols to be imported when using "from package import *"
-__all__ = ['attack', 'evaluate', 'load_data', 'standard_white_box_attack', 'surrogate_model', 'test_accuracy']  
+__all__ = [
+    "attack",
+    "evaluate",
+    "load_data",
+    "surrogate_model",
+    "test_accuracy",
+]
+
 
 # Initialization setup (package-wide configuration)
 def setup_package():
@@ -34,5 +37,6 @@ def setup_package():
     when the package is imported.
     """
     print("AIML package ({})is being initialized.".format(__version__))
+
 
 setup_package()
