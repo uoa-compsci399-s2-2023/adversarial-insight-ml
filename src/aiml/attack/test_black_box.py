ef1d1a112aad0b9bfb4d4448d85c66b4d3fcdbae
Sungjae Jang <jwj981417@gmail.com>
2023-09-28 04:57:42 +1300

    Fix imports
    Fix incorrect imports, and use specific imports

--- src/aiml/attack/test_black_box.py
+++ src/aiml/attack/test_black_box.py
@@ -3,8 +3,8 @@ test_black_box.py
 """
 
 
-from surrogate_model.create_surrogate_model import *
-from attack.test_white_box import *
+from aiml.surrogate_model.create_surrogate_model import create_surrogate_model
+from aiml.attack.test_white_box import test_all_white_box_attack
 
 def black_box_test(black_box_model):
     #TODO: Implement black-box attack testing
