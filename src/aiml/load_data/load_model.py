b3842f5d9265727fe5c1507e4d5a6e096a3c5b4f
Sungjae Jang <jwj981417@gmail.com>
2023-09-30 04:56:38 +1300

    Update load_model.py

--- src/aiml/load_data/load_model.py
+++ src/aiml/load_data/load_model.py
@@ -5,5 +5,12 @@ This script is responsible for loading the model.
 """
 
 
-def load_model(model):
+import detectors
+
+
+def load_model(model, device):
+    if type(model) == type("a"):
+        model = detectors.create_model(model, pretrained=True)
+        model = model.to(device)
+        
     return model
