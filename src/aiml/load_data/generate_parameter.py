d2e31ad63caf4d9ba4b9094abd7baee1bb5634a0
Sungjae Jang <jwj981417@gmail.com>
2023-09-30 04:58:50 +1300

    Reformat code

--- src/aiml/load_data/generate_parameter.py
+++ src/aiml/load_data/generate_parameter.py
@@ -8,7 +8,10 @@ This module provides a function for generating parameters based on input data an
 import torch
 import numpy as np
 
-def generate_parameter(input_shape,clip_values,nb_classes,dataset_test,dataloader_test):
+
+def generate_parameter(
+    input_shape, clip_values, nb_classes, dataset_test, dataloader_test
+):
     if input_shape == None:
         (x, y) = next(iter(dataset_test))
         input_shape = np.array(x.size())
@@ -16,22 +19,23 @@ def generate_parameter(input_shape,clip_values,nb_classes,dataset_test,dataloade
 
     if clip_values == None:
         global_min = np.inf
-        global_max = (-1)*np.inf
+        global_max = (-1) * np.inf
         s = 0
         n = 0
         b = True
         for batch in dataloader_test:
             x, _ = batch
-            if b == True:
-                ##print(batch)
+            if b:
                 b = False
             global_min = min(torch.min(x).item(), global_min)
             global_max = max(torch.max(x).item(), global_max)
-        clip_values = (global_min,global_max)
+        
+        clip_values = (global_min, global_max)
 
         ##print(f'Min: {global_min}, Max: {global_max}')
 
     if nb_classes == None:
-        list1 = set([y for _,y in dataset_test]) # use set to get unique classes
-        nb_classes=len(list1)
-    return (input_shape,clip_values,nb_classes)
+        list1 = set([y for _, y in dataset_test])  # use set to get unique classes
+        nb_classes = len(list1)
+    
+    return (input_shape, clip_values, nb_classes)
