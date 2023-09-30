c554b3c5ce94088c051ce342978f267396263ad5
zhou920ming <83534031+zhou920ming@users.noreply.github.com>
2023-09-30 12:12:29 +0800

    Update load_test_set.py

--- src/aiml/load_data/load_test_set.py
+++ src/aiml/load_data/load_test_set.py
@@ -8,7 +8,7 @@ This script is responsible for loading the test dataset for model evaluation.
 import torch
 from torchvision import transforms
 
-from surrogate_model.utils import load_cifar10
+from aiml.surrogate_model.utils import load_cifar10
 
 def load_test_set(test_set, batch_size_test):
     if type(test_set) == type("a"):
