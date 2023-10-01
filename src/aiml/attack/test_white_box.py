9ba757d4cb6ec8d42e4492cc3cfd9bf91b1f18e7
Sungjae Jang <jwj981417@gmail.com>
2023-10-02 05:59:15 +1300

    Update test_white_box.py

--- src/aiml/attack/test_white_box.py
+++ src/aiml/attack/test_white_box.py
@@ -5,6 +5,7 @@ This module contains functions for testing white-box attack methods on a
 PyTorch classifier model, and returns respecitve accuracies in a list.
 """
 
+
 import os
 
 import torch
@@ -12,7 +13,7 @@ from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
 import torchvision.transforms as T
 
-from aiml.evaluation.test_accuracy import test_accuracy2
+from aiml.evaluation.test_accuracy import test_accuracy
 from aiml.attack.adversarial_attacks import *
 
 
@@ -39,25 +40,39 @@ def test_white_box_attack(
 
     # Create the attack instance
     attack_method_list = [
-        [0, auto_projected_cross_entropy, [[16], [20], [32]],"auto_projected_cross_entropy",["batch"]],
-        [1, auto_projected_difference_logits_ratio, [[1], [16], [32]],"auto_projected_difference_logits_ratio",["batch"]],
-        [2, carlini_L0_attack, [[1], [16], [32]],"carlini_L0_attack",["batch"]],
-        [3, carlini_L2_attack, [[1], [16], [32]],"carlini_L2_attack",["batch"]],
-        [4, carlini_Linf_attack, [[1], [16], [32]],"carlini_Linf_attack",["batch"]],
-        [5, deep_fool_attack, [[1], [16], [32]],"deep_fool_attack",["batch"]],
-        [6, pixel_attack, [[None]],"pixel_attack",["th"]],
-        [7, square_attack, [[1], [16], [32]],"square_attack",["batch"]],
-        [8, zoo_attack, [[1], [16], [32]],"zoo_attack",["batch"]],
+        [
+            0,
+            auto_projected_cross_entropy,
+            [[16], [20], [32]],
+            "auto_projected_cross_entropy",
+            ["batch"],
+        ],
+        [
+            1,
+            auto_projected_difference_logits_ratio,
+            [[1], [16], [32]],
+            "auto_projected_difference_logits_ratio",
+            ["batch"],
+        ],
+        [2, carlini_L0_attack, [[1], [16], [32]], "carlini_L0_attack", ["batch"]],
+        [3, carlini_L2_attack, [[1], [16], [32]], "carlini_L2_attack", ["batch"]],
+        [4, carlini_Linf_attack, [[1], [16], [32]], "carlini_Linf_attack", ["batch"]],
+        [5, deep_fool_attack, [[1], [16], [32]], "deep_fool_attack", ["batch"]],
+        [6, pixel_attack, [[None]], "pixel_attack", ["th"]],
+        [7, square_attack, [[1], [16], [32]], "square_attack", ["batch"]],
+        [8, zoo_attack, [[1], [16], [32]], "zoo_attack", ["batch"]],
     ]
-    para=attack_method_list[attack_n][2][para_n]
-    if len(para)==1:
-        attack = attack_method_list[attack_n][1](classifer,para[0])
-    elif len(para)==2:
-        attack = attack_method_list[attack_n][1](classifer,para[0],para[1])
-    elif len(para)==3:
-        attack = attack_method_list[attack_n][1](classifer,para[0],para[1],para[2])
+    para = attack_method_list[attack_n][2][para_n]
+    if len(para) == 1:
+        attack = attack_method_list[attack_n][1](classifer, para[0])
+    elif len(para) == 2:
+        attack = attack_method_list[attack_n][1](classifer, para[0], para[1])
+    elif len(para) == 3:
+        attack = attack_method_list[attack_n][1](classifer, para[0], para[1], para[2])
     else:
-        attack = attack_method_list[attack_n][1](classifer,para[0],para[1],para[2],para[3])
+        attack = attack_method_list[attack_n][1](
+            classifer, para[0], para[1], para[2], para[3]
+        )
     X = []
     y = []
 
@@ -89,18 +104,18 @@ def test_white_box_attack(
         if all_zero:
             enough = True
 
-        
-
     X = np.array(X)
     y = np.array(y)
 
     y = torch.from_numpy(y)
+
     # Generate adversarial examples
     X_advx = attack.generate(X)
     print("ori", len(X), type(X))
     print("adv", len(X_advx), type(X))
     X_tensor = torch.Tensor(X)
     X_advx_tensor = torch.Tensor(X_advx)
+
     # Create a TensorDataset for the adversarial examples
     dataset_advx = TensorDataset(torch.Tensor(X_advx), y)
 
@@ -113,9 +128,9 @@ def test_white_box_attack(
     )  # high num_workers may cause err
 
     # Test the model's accuracy on the adversarial examples
-    # acc_advx = test_accuracy(model, dataloader_advx, device)
-    acc_advx, correct_advx = test_accuracy2(model, dataloader_advx, device)
+    acc_advx, correct_advx = test_accuracy(model, dataloader_advx, device)
     print(correct_advx, len(correct_advx))
+
     # generate image
     print(len(correct_advx))
     for i in range(len(correct_advx[0])):
@@ -132,7 +147,9 @@ def test_white_box_attack(
             transform = T.ToPILImage()
             orig_img = transform(X_tensor[i])
             advx_img = transform(X_advx_tensor[i])
-            img_path = f"./img/{attack_method_list[attack_n][3]}/{para_n}/{y[i]}/succeed"
+            img_path = (
+                f"./img/{attack_method_list[attack_n][3]}/{para_n}/{y[i]}/succeed"
+            )
             if not os.path.exists(img_path):
                 os.makedirs(img_path)
             orig_img.save(f"{img_path}/{i}orignial.png", "PNG")
