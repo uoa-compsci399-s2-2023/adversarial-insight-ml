a31b3199d4e14280045bef69a1555b587e6bdf21
Sungjae Jang <jwj981417@gmail.com>
2023-10-03 07:05:10 +1300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
 project = 'Adversarial Insight ML (AIML)'
 copyright = '2023, Team 7'
 author = 'Team 7'
