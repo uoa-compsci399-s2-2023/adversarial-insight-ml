80ca8f2cd0cf1c3c15a73df8aca1bc3d73b0b9fb
Sungjae Jang <jwj981417@gmail.com>
2023-10-02 23:37:26 +1300

    Update import and function call
    This commit reflects changes to the function name from [test_white_box_attack] to [test_attack]. It ensures code consistency.

--- src/aiml/evaluation/evaluate.py
+++ src/aiml/evaluation/evaluate.py
@@ -13,7 +13,7 @@ from aiml.load_data.load_model import load_model
 from aiml.load_data.load_test_set import load_test_set
 from aiml.load_data.load_test_set import load_train_set
 from aiml.load_data.generate_parameter import generate_parameter
-from aiml.attack.test_attack import test_white_box_attack
+from aiml.attack.test_attack import test_attack
 from aiml.evaluation.test_accuracy import test_accuracy
 from aiml.evaluation.dynamic import decide_attack
 from aiml.surrogate_model.create_surrogate_model import create_surrogate_model
@@ -100,7 +100,7 @@ def evaluate(
             [
                 current_attack_n,
                 para_n,
-                test_white_box_attack(
+                test_attack(
                     current_attack_n,
                     para_n,
                     model,
