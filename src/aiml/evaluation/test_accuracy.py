f3d3ddf8ac93fd9a5e22a6eeb089da3937d48115
Sungjae Jang <jwj981417@gmail.com>
2023-10-02 18:02:50 +1300

    Restore separate test_accuracy functions

--- src/aiml/evaluation/test_accuracy.py
+++ src/aiml/evaluation/test_accuracy.py
@@ -10,6 +10,30 @@ import torch
 
 
 def test_accuracy(model, dataloader, device):
+    """This function returns the accuracy of a given dataset on a pre-trained model."""
+    correct = 0
+    total = 0
+    model.eval()
+    
+    with torch.no_grad():
+        for batch in dataloader:
+            x, y = batch
+            x = x.to(device)
+
+            outputs = model(x)
+
+            _, predictions = torch.max(outputs, 1)
+            predictions = predictions.to("cpu")
+
+            total += y.size(0)
+            correct += (predictions == y).sum().item()
+
+    accuracy = correct / total
+
+    return accuracy
+
+
+def test_accuracy_with_flags(model, dataloader, device):
     correct = 0
     total = 0
 
