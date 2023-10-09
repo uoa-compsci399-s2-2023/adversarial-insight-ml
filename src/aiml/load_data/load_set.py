1107de0becaf11448e12d91f4122b414bf128a93
Sungjae Jang <jwj981417@gmail.com>
2023-10-10 07:14:10 +1300

    Update docstring and reformat code in load_set.py

--- src/aiml/load_data/load_set.py
+++ src/aiml/load_data/load_set.py
@@ -5,50 +5,45 @@ This script is responsible for loading the test dataset for model evaluation.
 """
 
 
-import torch
-
 from aiml.surrogate_model.utils import load_cifar10
-"""
-The function is for loading test data set
-args:
-dataset(dataset/string):if input is string, it will find the target dataset
 
-return 
-dataset,
-
-"""
 
 def load_test_set(test_set):
+    """
+    Load a test dataset.
+
+    Parameters:
+        dataset (dataset or string): If a string is provided, it will search for the target dataset.
+
+    Returns:
+        dataset: The loaded test dataset.
+    """
     if type(test_set) == type("a"):
         if test_set == "cifar10":
             dataset_train = load_cifar10(train=True, require_normalize=True)
         else:
-            print("currently we cannot find the dataset you input")
+            print("Currently we cannot find the dataset you input.")
             return None
 
     
     return test_set
-"""
-The function is for loading train data set
-args:
-dataset(dataset/string):if input is string, it will find the target dataset
 
-return 
-dataset,
-
-"""
 
 def load_train_set(test_set):
+    """
+    Load a training dataset.
+
+    Parameters:
+        dataset (dataset or string): If a string is provided, it will search for the target dataset.
+
+    Returns:
+        dataset: The loaded training dataset.
+    """
     if type(test_set) == type("a"):
         if test_set == "cifar10":
             dataset_train = load_cifar10(train=False, require_normalize=True)
         else:
-            print("currently we cannot find the dataset you input")
+            print("Currently we cannot find the dataset you input.")
             return None
-
-  
-    
-
-    
 
     return test_set
