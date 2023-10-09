f506b9d579ec17ba5fdc520770223cd33f105a6e
Sungjae Jang <jwj981417@gmail.com>
2023-10-10 06:19:35 +1300

    Fix import
    Fixes import for changed filename

--- src/aiml/evaluation/evaluate.py
+++ src/aiml/evaluation/evaluate.py
@@ -13,7 +13,7 @@ from art.estimators.classification import PyTorchClassifier
 
 from aiml.load_data.generate_parameter import generate_parameter
 from aiml.load_data.normalize_datasets import normalize_datasets
-from aiml.attack.test_attack import test_attack
+from aiml.attack.attack_evaluation import attack_evaluation
 from aiml.evaluation.test_accuracy import test_accuracy
 from aiml.evaluation.dynamic import decide_attack
 from aiml.surrogate_model.create_surrogate_model import create_surrogate_model
