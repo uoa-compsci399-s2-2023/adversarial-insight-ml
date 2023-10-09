59ce60f4ecabee145661eb42242335491d7bb02a
zhou920ming <83534031+zhou920ming@users.noreply.github.com>
2023-10-09 08:56:55 +0800

    Update load_test_set.py

--- src/aiml/load_data/load_test_set.py
+++ src/aiml/load_data/load_test_set.py
@@ -12,62 +12,43 @@ from aiml.surrogate_model.utils import load_cifar10
 The function is for loading test data set
 args:
 dataset(dataset/string):if input is string, it will find the target dataset
-batch_size_test(int)
+
 return 
 dataset,
-dataset_loader
+
 """
 
-def load_test_set(test_set, batch_size_test):
+def load_test_set(test_set):
     if type(test_set) == type("a"):
         if test_set == "cifar10":
             dataset_train = load_cifar10(train=True, require_normalize=True)
         else:
             print("currently we cannot find the dataset you input")
-            return None, None
+            return None
 
-    loader = torch.utils.data.DataLoader(
-        test_set, batch_size=batch_size_test, shuffle=False
-    )
-
-    data = next(iter(loader))
-    mean = data[0].mean()
-    std = data[0].std()
-
-    if mean > 0.1 or mean < -0.1 or std > 1.1 or std < 0.9:
-        print("the dataset is unnormalized dataset")
-    else:
-        print("the dataset is normalized dataset")
-    return test_set, loader
+    
+    return test_set
 """
 The function is for loading train data set
 args:
 dataset(dataset/string):if input is string, it will find the target dataset
-batch_size_test(int)
+
 return 
 dataset,
-dataset_loader
+
 """
 
-def load_train_set(test_set, batch_size_test):
+def load_train_set(test_set):
     if type(test_set) == type("a"):
         if test_set == "cifar10":
             dataset_train = load_cifar10(train=False, require_normalize=True)
         else:
             print("currently we cannot find the dataset you input")
-            return None, None
+            return None
 
-    loader = torch.utils.data.DataLoader(
-        test_set, batch_size=batch_size_test, shuffle=False
-    )
+  
+    
 
-    data = next(iter(loader))
-    mean = data[0].mean()
-    std = data[0].std()
+    
 
-    if mean > 0.1 or mean < -0.1 or std > 1.1 or std < 0.9:
-        print("the dataset is unnormalized dataset")
-    else:
-        print("the dataset is normalized dataset")
-
-    return test_set, loader
+    return test_set
