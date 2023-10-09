9193493a137d82742214081d25a30a1640e8bdbc
Sungjae Jang <jwj981417@gmail.com>
2023-10-10 07:08:32 +1300

    Update docstring in generate_parameter.py

--- src/aiml/load_data/generate_parameter.py
+++ src/aiml/load_data/generate_parameter.py
@@ -7,25 +7,24 @@ This module provides a function for generating parameters based on input dataset
 
 import torch
 import numpy as np
-"""
-generate the images shape, clip values and the number of classes based on input dataset.
-args:
- input_shape(nparray):images shape, if it is given(not None) it will not change
- clip_values(tuple):the range of the data, if it is given(not None) it will not change
- nb_classes(int):the number of classes, if it is given(not None) it will not change
- dataset_test:given dataset
- dataloader_test:the loader of the given dataset
-return:
-input_shape(nparray):images shape
- clip_values(tuple):the range of the data
- nb_classes(int):the number of classes
- 
 
-"""
 
 def generate_parameter(
     input_shape, clip_values, nb_classes, dataset_test, dataloader_test
 ):
+    """
+    Generate the image shape, clip values, and number of classes based on the input dataset.
+
+    Parameters:
+        input_shape (numpy array): Image shape; if given (not None), it will remain unchanged.
+        clip_values (tuple): Data range; if given (not None), it will remain unchanged.
+        nb_classes (int): Number of classes; if given (not None), it will remain unchanged.
+        dataset_test: The input dataset for reference.
+        dataloader_test: DataLoader for the input dataset.
+
+    Returns:
+        tuple: A tuple containing (input_shape, clip_values, nb_classes).
+    """
     if input_shape == None:
         (x, y) = next(iter(dataset_test))
         input_shape = np.array(x.size())
