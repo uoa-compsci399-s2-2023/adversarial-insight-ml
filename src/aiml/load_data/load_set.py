9c50fdab4d360c1714e12cd931c838980eb00f11
zhou920ming <83534031+zhou920ming@users.noreply.github.com>
2023-10-13 08:45:22 +0800

    Update load_set.py

--- src/aiml/load_data/load_set.py
+++ src/aiml/load_data/load_set.py
@@ -1,3 +1,5 @@
+
+
 """
 load_set.py
 
@@ -6,7 +8,8 @@ This script is responsible for loading the test dataset for model evaluation.
 
 
 from aiml.surrogate_model.utils import load_cifar10
-
+from datasets import list_datasets
+from datasets import load_dataset
 
 def load_test_set(test_set):
     """
@@ -20,16 +23,21 @@ def load_test_set(test_set):
     """
     if type(test_set) == type("a"):
         if test_set == "cifar10":
-            dataset_train = load_cifar10(train=True, require_normalize=True)
+            test_set = load_cifar10(train=False, require_normalize=True)
+            
         else:
-            print("Currently we cannot find the dataset you input.")
-            return None
+            try:
+                test_set = load_dataset(path=test_set, split='test')
+                print("we find the test set on huggingface")
+            except:
+                print("Currently we cannot find the test dataset you input. you can call huggingface_hub.list_datasets to see the whole valiable set")
+                return None
 
     
     return test_set
 
 
-def load_train_set(test_set):
+def load_train_set(train_set):
     """
     Load a training dataset.
 
@@ -39,11 +47,18 @@ def load_train_set(test_set):
     Returns:
         dataset: The loaded training dataset.
     """
-    if type(test_set) == type("a"):
-        if test_set == "cifar10":
-            dataset_train = load_cifar10(train=False, require_normalize=True)
+    if type(train_set) == type("a"):
+        if train_set == "cifar10":
+            train_set = load_cifar10(train=True, require_normalize=True)
+            
         else:
-            print("Currently we cannot find the dataset you input.")
-            return None
+            try:
+                train_set = load_dataset(path=train_set, split='train')
+                print("we find the train set on huggingface")
+            except:
+                print("Currently we cannot find the train dataset you input. you can call huggingface_hub.list_datasets to see the whole valiable set")
+                return None
 
-    return test_set
+    
+    return train_set
+
