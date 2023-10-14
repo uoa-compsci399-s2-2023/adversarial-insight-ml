142d35f3b2c28517776a6396b005092b8bbfed1c
zhou920ming <83534031+zhou920ming@users.noreply.github.com>
2023-10-14 19:18:46 +0800

    Update load_set.py

--- src/aiml/load_data/load_set.py
+++ src/aiml/load_data/load_set.py
@@ -6,7 +6,9 @@ This script is responsible for loading the test dataset for model evaluation.
 
 
 from aiml.surrogate_model.utils import load_cifar10
-
+from datasets import list_datasets
+from datasets import load_dataset
+import torchvision as tv
 
 def load_test_set(test_set):
     """
@@ -20,16 +22,24 @@ def load_test_set(test_set):
     """
     if type(test_set) == type("a"):
         if test_set == "cifar10":
-            dataset_train = load_cifar10(train=True, require_normalize=True)
+            test_set = load_cifar10(train=False, require_normalize=True)
+        elif train_set =="mnist":
+            train_set=tv.datasets.MNIST('./data', download=True, train=False)
+        elif train_set == "cifar100":
+            train_set = tv.datasets.CIFAR100('./data', download=True, train=False)
         else:
-            print("Currently we cannot find the dataset you input.")
-            return None
+            try:
+                test_set = load_dataset(path=test_set, split='test')
+                print("we find the test set on huggingface")
+            except:
+                print("Currently we cannot find the test dataset you input. you can call huggingface_hub.list_datasets to see the whole valiable set")
+                return None
 
     
     return test_set
 
 
-def load_train_set(test_set):
+def load_train_set(train_set):
     """
     Load a training dataset.
 
@@ -39,11 +49,20 @@ def load_train_set(test_set):
     Returns:
         dataset: The loaded training dataset.
     """
-    if type(test_set) == type("a"):
-        if test_set == "cifar10":
-            dataset_train = load_cifar10(train=False, require_normalize=True)
+    if type(train_set) == type("a"):
+        if train_set == "cifar10":
+            train_set = tv.datasets.CIFAR10('./data', download=True, train=True)
+        elif train_set =="mnist":
+            train_set=tv.datasets.MNIST('./data', download=True, train=True)
+        elif train_set == "cifar100":
+            train_set = tv.datasets.CIFAR100('./data', download=True, train=True)
         else:
-            print("Currently we cannot find the dataset you input.")
-            return None
+            try:
+                train_set = load_dataset(path=train_set, split='train')
+                print("we find the train set on huggingface")
+            except:
+                print("Currently we cannot find the train dataset you input. you can call huggingface_hub.list_datasets to see the whole valiable set")
+                return None
 
-    return test_set
+    
+    return train_set
