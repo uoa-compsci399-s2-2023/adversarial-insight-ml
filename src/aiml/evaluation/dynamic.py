203d58d3a26c652ff48f24567b5c8950740c5f36
Sungjae Jang <jwj981417@gmail.com>
2023-10-18 17:46:45 +1300

    Reformat dynamic.py

--- src/aiml/evaluation/dynamic.py
+++ src/aiml/evaluation/dynamic.py
@@ -19,20 +19,13 @@ from aiml.attack.adversarial_attacks import (
 )
 
 
-def decide_attack(
-    result_list,
-    attack_para_list=[
-    
-    ],
-    now_time="0",
-    ori_acc=0.9
-):
+def decide_attack(result_list, attack_para_list=[], now_time="0", ori_acc=0.9):
     """
-    Write the results of the previous attack to a text file and determine 
+    Write the results of the previous attack to a text file and determine
     the next attack and its parameters based on attack history.
 
     Parameters:
-        result_list: A list where the first element is the overall mark, and the subsequent 
+        result_list: A list where the first element is the overall mark, and the subsequent
             elements are lists containing the history of previous attacks.
             Sublists stores the attack number, parameter number, and accuracy.
        attack_para_list: list that store parameter for each attack
@@ -40,11 +33,11 @@ def decide_attack(
        ori_acc(float): original accuracy(accuracy that test clean image by the model)
 
     Returns:
-        next_attack_number (int): The number of the next attack 
+        next_attack_number (int): The number of the next attack
             (could be the same or the next one in the attack_method_list).
         next_parameter_number (int): The number of the next parameter.
         continue_testing (bool): Whether to continue testing attacks or not.
-        
+
     """
 
     attack_method_list = [
@@ -53,24 +46,24 @@ def decide_attack(
             auto_projected_cross_entropy,
             attack_para_list[0],
             "auto_projected_cross_entropy",
-            ["eps","batch",  "eps_step"],
+            ["eps", "batch", "eps_step"],
         ],
         [
             1,
             auto_projected_difference_logits_ratio,
             attack_para_list[1],
             "auto_projected_difference_logits_ratio",
-            ["eps","batch",  "eps_step"],
+            ["eps", "batch", "eps_step"],
         ],
         [
             2,
             carlini_L0_attack,
             attack_para_list[2],
             "carlini_L0_attack",
-            [   "confidence",
+            [
+                "confidence",
                 "batch",
                 "learning_rate",
-                
             ],
         ],
         [
@@ -82,7 +75,6 @@ def decide_attack(
                 "confidence",
                 "batch",
                 "learning_rate",
-                
             ],
         ],
         [
@@ -94,7 +86,6 @@ def decide_attack(
                 "confidence",
                 "batch",
                 "learning_rate",
-                
             ],
         ],
         [
@@ -102,7 +93,7 @@ def decide_attack(
             deep_fool_attack,
             attack_para_list[5],
             "deep_fool_attack",
-            ["epsilon","batch", "max_iter"],
+            ["epsilon", "batch", "max_iter"],
         ],
         [
             6,
@@ -116,7 +107,7 @@ def decide_attack(
             square_attack,
             attack_para_list[7],
             "square_attack",
-            ["eps","batch", "max_iter"],
+            ["eps", "batch", "max_iter"],
         ],
         [
             8,
@@ -128,11 +119,10 @@ def decide_attack(
                 "batch",
                 "learning_rate",
                 "max_iter",
-                
             ],
         ],
     ]
-    
+
     """
     attack_method_list contains all eight adversarial attack methods used.
 
@@ -149,23 +139,20 @@ def decide_attack(
     - Three possible parameter choices exist: batch=16, batch=20, or batch=32.
     """
 
-
-    if result_list==[]:  # add the first attack to initial result list
+    if result_list == []:  # add the first attack to initial result list
         return (
             0,
             0,
             True,
-            
         )  # current_attack_n,para,current_attack,b
 
-    
     previous = result_list[-1]  # get information of previous attack result
     previous_attack_n = previous[0]
     previous_acc = previous[2]
     previous_para_n = previous[1]
-    file_name="attack_evaluation_result"+str(now_time)+".txt"
+    file_name = "attack_evaluation_result" + str(now_time) + ".txt"
     with open(
-        file_name, "a" #the result will output to attack evaluation result.txt
+        file_name, "a"  # the result will output to attack evaluation result.txt
     ) as f:  # write the results of previous attack to txt file
         f.write(attack_method_list[previous_attack_n][3])
         f.write("    ")
@@ -188,38 +175,46 @@ def decide_attack(
     on average. It will skip the weak attack later.
     """
 
-    if (
-        previous_acc < ori_acc*0.4
-        or previous_para_n +1>= len(attack_method_list[previous_attack_n][2]) 
+    if previous_acc < ori_acc * 0.4 or previous_para_n + 1 >= len(
+        attack_method_list[previous_attack_n][2]
     ):
-        strong=(previous_para_n +1)/len(attack_method_list[previous_attack_n][2])
-        out_string=""
-        if strong>=0.8 and previous_acc >= ori_acc*0.4:
-            out_string="your model is very robust on "+str(attack_method_list[previous_attack_n][3])+"\n"
-        elif strong>=0.5 and previous_acc >= ori_acc*0.2:
-            out_string="your model is barely robust on "+str(attack_method_list[previous_attack_n][3])+"\n"
+        strong = (previous_para_n + 1) / len(attack_method_list[previous_attack_n][2])
+        out_string = ""
+        if strong >= 0.8 and previous_acc >= ori_acc * 0.4:
+            out_string = (
+                "Your model is very robust on "
+                + str(attack_method_list[previous_attack_n][3])
+                + "\n"
+            )
+        elif strong >= 0.5 and previous_acc >= ori_acc * 0.2:
+            out_string = (
+                "Your model is barely robust on "
+                + str(attack_method_list[previous_attack_n][3])
+                + "\n"
+            )
         else:
-            out_string="your model is not robust on "+str(attack_method_list[previous_attack_n][3])+"\n"
+            out_string = (
+                "Your model is not robust on "
+                + str(attack_method_list[previous_attack_n][3])
+                + "\n"
+            )
         next_para_n = 0
         with open(
-            file_name, "a" #the result will output to attack evaluation result.txt
+            file_name, "a"  # the result will output to attack evaluation result.txt
         ) as f:  # write the results of previous attack to txt file
-            f.write(out_string+"\n")
+            f.write(out_string + "\n")
         if previous_attack_n < 8:
-            
             next_para_n = 0
             return (
                 previous_attack_n + 1,
                 next_para_n,
                 True,
-                
             )
         else:  # all attack are tested and finish
             return (
                 0,
                 0,
                 False,
-                
             )
 
     else:
@@ -227,5 +222,4 @@ def decide_attack(
             previous_attack_n,
             previous_para_n + 1,
             True,
-            
         )
