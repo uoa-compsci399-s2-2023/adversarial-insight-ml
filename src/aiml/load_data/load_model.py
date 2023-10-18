0a41b96486f30b31f6396202ce369af8386abc63
zhou920ming <83534031+zhou920ming@users.noreply.github.com>
2023-10-18 10:01:10 +0800

    Update load_model.py

--- src/aiml/load_data/load_model.py
+++ src/aiml/load_data/load_model.py
@@ -3,7 +3,7 @@ load_model.py
 
 This script is responsible for loading the model.
 """
-
+import timm 
 
 def load_model(model, device):
     """
@@ -20,7 +20,11 @@ def load_model(model, device):
 
     if type(model) == type("a"):
         try:
-            # model = detectors.create_model(model, pretrained=True)
+            
+
+            model = timm.create_model(model,pretrained=True)
+
+
             model = model.to(device)
         except:
             from robustbench.utils import load_model
