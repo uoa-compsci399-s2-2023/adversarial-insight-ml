0e747b6e0a93a3247144716c3511dfa71e2ac7ac
Sungjae Jang <jwj981417@gmail.com>
2023-10-18 17:49:02 +1300

    Reformat load_model.py

--- src/aiml/load_data/load_model.py
+++ src/aiml/load_data/load_model.py
@@ -3,14 +3,16 @@ load_model.py
 
 This script is responsible for loading the model.
 """
-import timm 
+import timm
 import torch
+
+
 def load_model(model, device):
     """
     Load a machine learning model.
 
     Parameters:
-        model (model or string): If a string is provided, it will search for 
+        model (model or string): If a string is provided, it will search for
             the target model by detectors.
         device (string): The device to use, either 'cpu' or 'gpu'.
 
@@ -19,18 +21,17 @@ def load_model(model, device):
     """
 
     if type(model) == type("a"):
-        if model[:5]=="cifar":
-            
-            model = torch.hub.load("chenyaofo/pytorch-cifar-models", model, pretrained=True)
+        if model[:5] == "cifar":
+            model = torch.hub.load(
+                "chenyaofo/pytorch-cifar-models", model, pretrained=True
+            )
         else:
             try:
-
-                model = timm.create_model(model,pretrained=True)
+                model = timm.create_model(model, pretrained=True)
 
                 model = model.to(device)
             except:
-                
-                    print("We can't find your model.")
-                    return None
+                print("We can't find your model.")
+                return None
 
     return model
